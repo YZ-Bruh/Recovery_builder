517af99a8fa85eb40350af182ad300a63720be54
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-07-31 14:41:19 +0300

    config.sh

--- config.sh
+++ config.sh
@@ -2,14 +2,14 @@
 
 # about the custom recovery
 export NAME="twrp" # supported: pbrp, shrp, twrp
-export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git" # the link of manifest
-export BRANCH="twrp-11" # the branch of manifest
+export MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni" # the link of manifest
+export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
-export DEVICE="selene" # codename used in device tree
-export DT_LINK="https://github.com/YZ-Bruh/android_device_xiaomi_selene" # device tree link
-export DT_BRANCH="twrp" # device tree branch
-export VENDOR="xiaomi" # device manufacturer or vendor
+export DEVICE="tb8765ap1_bsp" # codename used in device tree
+export DT_LINK="https://github.com/YZ-Bruh/android_device_tcl_tb8765ap1_bsp" # device tree link
+export DT_BRANCH="full_A3A_10_4G-user-9-PPR1.180610.011-vL43-release-keys" # device tree branch
+export VENDOR="tcl" # device manufacturer or vendor
 export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="twrp" # recovery type: omni, twrp, something else
 
